1001e03ab373600450767b9afdd1e84941172a3d
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-09-11 16:21:53 -0500

    se realizo llamado de la api en login-email,login-password y implementacion token jwt

--- src/components/avatar/src/Avatar.jsx
+++ src/components/avatar/src/Avatar.jsx
@@ -1,13 +1,9 @@
 import '../styles/desktop.scss'
 import SloganAvatar from '../../sloganAvatar/src/SloganAvatar'
 import UserAvatar from 'app/components/userAvatar'
-import {useContext} from 'react'
-import {UserContext} from '../../../Context/index'
 
 const Avatar = username => {
   const {name = '', lastName = ''} = username ?? {}
-
-  const {account, setAccount} = useContext(UserContext)
 
   const defaultAvatar = 'contain-avatar'
 
