89f24934970092ba7907dc3f7a75fddd2f446824
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-11-06 21:07:55 -0500

    implementacion ItemMoneyTransfer  de perfil

--- src/app/cross/ui/components/ItemMoneyTransfer/src/ItemMoneyTransfer.jsx
+++ src/app/cross/ui/components/ItemMoneyTransfer/src/ItemMoneyTransfer.jsx
@@ -3,10 +3,11 @@ import '../styles/desktop.scss'
 import Image from 'next/image'
 
 const ItemMoneyTransfer = ({cvTitle, descriptionCV}) => {
+  const defaultItemMoney = 'ItemMoneyTransfer'
   return (
-    <div className="ItemMoneyTransfer">
-      <p>{cvTitle}</p>
-      <p>{descriptionCV}</p>
+    <div className={`${defaultItemMoney}`}>
+      <p className={`${defaultItemMoney}__title`}>{cvTitle}</p>
+      <p className={`${defaultItemMoney}__description`}>{descriptionCV}</p>
       <Image src="/images/carbon:copy.svg" width={14} height={22} alt="copy" />
     </div>
   )
