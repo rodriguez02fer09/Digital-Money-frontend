b5e8ea0b0e3701436d41423499b4efc98131c7aa
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-11-19 16:00:02 -0500

    ajustes mobile estilos

--- src/app/cross/ui/components/sloganAvatar/src/SloganAvatar.jsx
+++ src/app/cross/ui/components/sloganAvatar/src/SloganAvatar.jsx
@@ -2,18 +2,19 @@
 
 import '../styles/desktop.scss'
 
-const SloganAvatar = ({username}) => {
-  const FIRSTLATTER = 0
-  const {firstname = '', lastName = ''} = username ?? {}
+const SloganAvatar = ({}) => {
+  // const FIRSTLATTER = 0
+  // const {firstname = '', lastName = ''} = username ?? {}
 
-  const firstLaterName = name?.charAt(FIRSTLATTER)
-  const firstLaterLastName = lastName?.charAt(FIRSTLATTER)
+  // const firstLaterName = name?.charAt(FIRSTLATTER)
+  // const firstLaterLastName = lastName?.charAt(FIRSTLATTER)
   const defaultSlogan = 'contain-slogan'
   return (
     <div className={`${defaultSlogan}`}>
       <p>
-        {firstLaterName}
-        {firstLaterLastName}
+        {/* {firstLaterName}
+        {firstLaterLastName} */}
+        FR
       </p>
     </div>
   )
