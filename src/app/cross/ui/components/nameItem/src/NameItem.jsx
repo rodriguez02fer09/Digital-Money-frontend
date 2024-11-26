92aaa9e2b3dd406bf0d594bfe0338308d8f44083
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-11-26 17:03:03 -0500

    fix styles dashboardwith menu

--- src/app/cross/ui/components/nameItem/src/NameItem.jsx
+++ src/app/cross/ui/components/nameItem/src/NameItem.jsx
@@ -1,7 +1,7 @@
 import '../styles/desktop.scss'
 import Image from 'next/image'
 
-const NameItem = ({nameItem}) => {
+const NameItem = () => {
   const defaultNameItem = 'nameItem'
   return (
     <div className={`${defaultNameItem}`}>
@@ -9,7 +9,7 @@ const NameItem = ({nameItem}) => {
         <Image src="/images/point.svg" width="25" height="24" />
       </div>
       <div className={`${defaultNameItem}__nameCard`}>
-        <p>{nameItem}</p>
+        <p>Transferiste a Rodrigo</p>
       </div>
     </div>
   )
