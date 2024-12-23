c56c54d123717732f0975c15785887c1fb555088
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2024-12-23 15:55:46 -0500

    ajustes de render de addCards

--- src/app/addCards/src/AddCards.jsx
+++ src/app/addCards/src/AddCards.jsx
@@ -2,10 +2,11 @@
 'use client'
 import '../styles/desktop.scss'
 import Image from 'next/image'
-import CardBlack from '../../cross/ui/components/cardBlack/'
+import CardBlack from '../../cross/ui/components/cardBlack'
+
 import CardActivity from '../../cross/ui/components/cardActivity'
 import ContainAddCards from '../../cross/ui/components/containAddCards'
-import ContainHistoryCards from 'app/app/cross/ui/components/containHistoryCards/src/ContainHistoryCards'
+import ContainHistoryCards from '../../cross/ui/components/containHistoryCards'
 
 const AddCards = () => {
   const defaultClass = 'add-cards'
---
+++ src/app/cross/ui/components/containHistoryCards/index.js
@@ -0,0 +1,3 @@
+import ContainHistoryCards from './src/ContainHistoryCards' // Ajusta la ruta según sea necesario
+
+export default ContainHistoryCards
