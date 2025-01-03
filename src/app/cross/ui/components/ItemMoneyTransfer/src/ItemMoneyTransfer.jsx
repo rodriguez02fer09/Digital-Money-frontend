d7e34b4d5675cb3c206e9efb8a86527556bc7d8f
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-01-03 17:27:06 -0500

    se realizo implementacion de copiar cvu y alias en el perfil

--- src/app/cross/ui/components/ItemMoneyTransfer/src/ItemMoneyTransfer.jsx
+++ src/app/cross/ui/components/ItemMoneyTransfer/src/ItemMoneyTransfer.jsx
@@ -1,17 +1,40 @@
 import '../styles/desktop.scss'
 
+import {toast} from 'react-toastify'
 import Image from 'next/image'
 
-const ItemMoneyTransfer = ({cvTitle, descriptionCV}) => {
+const ItemMoneyTransfer = ({title, value}) => {
+  const copyText = async text => {
+    try {
+      await navigator.clipboard.writeText(text)
+      toast.success(`Copiaste ${title} ${text}`, {
+        position: 'bottom-left',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'dark',
+      })
+    } catch (err) {}
+  }
+
   const defaultItemMoney = 'ItemMoneyTransfer'
   return (
     <div className={`${defaultItemMoney}`}>
       <div className={`${defaultItemMoney}__contain-title`}>
-        <p>{cvTitle}</p>
-        <Image src="/images/carbon.svg" width={24} height={24} alt="copy" />
+        <p>{title}</p>
+        <Image
+          src="/images/carbon.svg"
+          width={24}
+          height={24}
+          alt="copy"
+          onClick={() => copyText(value)}
+          style={{cursor: 'pointer'}} // Estilo para indicar interactividad
+        />
       </div>
       <div className={`${defaultItemMoney}__contain-description`}>
-        <p className={`${defaultItemMoney}--description`}>{descriptionCV}</p>
+        <p className={`${defaultItemMoney}--description`}>{value}</p>
       </div>
     </div>
   )
