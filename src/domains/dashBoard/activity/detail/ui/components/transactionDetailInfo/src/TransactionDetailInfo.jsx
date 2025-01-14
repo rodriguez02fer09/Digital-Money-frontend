bab13db9bb966e5db140b531c88b4e97f95d87ce
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-01-14 12:26:06 -0500

    estilos de componente de detalle de transacion

--- src/domains/dashBoard/activity/detail/ui/components/transactionDetailInfo/src/TransactionDetailInfo.jsx
+++ src/domains/dashBoard/activity/detail/ui/components/transactionDetailInfo/src/TransactionDetailInfo.jsx
@@ -1,37 +1,43 @@
 import CardBlack from '@domains/cross/ui/components/cardBlack'
 import '../styles/main.scss'
 import Image from 'next/image'
-import {classNames} from 'classnames'
 
 const TransactionDetailInfo = () => {
   const defaultDetail = 'detail-transaction'
 
   return (
     <CardBlack size="Detail">
-      <div className={`${defaultDetail}`}>
+      <div className={defaultDetail}>
         <div className={`${defaultDetail}__statusDetail`}>
-          <Image
-            src="/images/check.svg"
-            alt="Edit Icon"
-            width={22}
-            height={22}
-          />
-          <p>Aprobada</p>
+          <div className={`${defaultDetail}__statusDetail--statusInfo`}>
+            <Image
+              src="/images/check.svg"
+              alt="Ícono de estado"
+              width={22}
+              height={22}
+            />
+            <p className="status-title">Aprobada</p>
+          </div>
+          <p className="status-date">Creada el 17 de agosto 2022 a 16:34 hs.</p>
         </div>
+
         <div className={`${defaultDetail}__transferSummary`}>
-          <p>Transferencia de dinero</p>
-          <p>$1.266,57</p>
+          <p className="summary-title">Transferencia de dinero</p>
+          <p className="summary-amount">$1.266,57</p>
         </div>
+
         <div className={`${defaultDetail}__recipientDetail`}>
-          <p>Transferencia de dinero</p>
-          <p>$1.266,57</p>
+          <p className="recipient-title">Le transferiste a </p>
+          <p className="recipient-amount">Rodrigo Vaccaro</p>
         </div>
+
         <div className={`${defaultDetail}__approvalDetail`}>
-          <p>Transferencia de dinero</p>
-          <p>$1.266,57</p>
+          <p className="approval-title">Número de operación</p>
+          <p className="approval-amount">27903047281</p>
         </div>
       </div>
     </CardBlack>
   )
 }
+
 export default TransactionDetailInfo
