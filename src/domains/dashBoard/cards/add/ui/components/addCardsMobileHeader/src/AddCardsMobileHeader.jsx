5a6ad465bc46095e92bd5201e4141c08a25a85ba
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-01-15 11:24:55 -0500

    ajustes generales de estilos

--- src/domains/dashBoard/cards/add/ui/components/addCardsMobileHeader/src/AddCardsMobileHeader.jsx
+++ src/domains/dashBoard/cards/add/ui/components/addCardsMobileHeader/src/AddCardsMobileHeader.jsx
@@ -1,14 +1,14 @@
 import '../styles/main.scss'
 import Image from 'next/image'
 
-const AddCardsMobileHeader = () => {
+const MobileHeader = ({title}) => {
   const defaultHeader = 'mobile-header'
   return (
     <div className={defaultHeader}>
       <Image src="/images/arrowBlack.svg" width={14} height={22} alt="Arrow" />
-      <p>Tarjetas</p>
+      <p>{title}</p>
     </div>
   )
 }
 
-export default AddCardsMobileHeader
+export default MobileHeader
