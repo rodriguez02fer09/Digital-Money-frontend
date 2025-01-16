9a0c263aa7460ee6c336279a17a25d366ae01eff
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-01-16 13:33:50 -0500

    implementacion selectCard en los diferentes dispositivos

--- src/domains/dashBoard/depositMoney/cardSelect/ui/composite-wrappers/cardSelect/src/CWCardSelect.jsx
+++ src/domains/dashBoard/depositMoney/cardSelect/ui/composite-wrappers/cardSelect/src/CWCardSelect.jsx
@@ -3,14 +3,21 @@ import CardBlack from '@domains/cross/ui/components/cardBlack/src/CardBlack'
 import Title from '@domains/dashBoard/depositMoney/cardSelect/ui/components/title/src/Title'
 import HistoryCards from '@domains/dashBoard/cards/list/ui/components/historyCards/src/HistoryCards'
 import ButtonAddCard from '@domains/dashBoard/depositMoney/cardSelect/ui/components/buttonAddCard/src/buttonAddCard'
-const CWCardSelect = () => {
+import MobileHeader from '@domains/cross/ui/components/MobileHeader/src/MobileHeader'
+import Button from '@domains/cross/ui/components/button/src/Button'
+
+const CWCardSelect = ({title}) => {
   return (
     <>
+      <MobileHeader title="Cargar dinero" />
       <CardBlack size="SelectCard">
         <Title />
         <HistoryCards />
         <ButtonAddCard />
       </CardBlack>
+      <div className="button">
+        <Button size="medium" color="green" label="Continuar" />
+      </div>
     </>
   )
 }
