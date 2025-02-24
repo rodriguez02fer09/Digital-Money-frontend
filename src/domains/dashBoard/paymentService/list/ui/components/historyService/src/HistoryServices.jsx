b6c81bc80023f17caafd62c0cdf47e40c026dbe1
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-02-24 11:13:02 -0500

    implementacion de pago de servicios page

---
+++ src/domains/dashBoard/paymentService/list/ui/components/historyService/src/HistoryServices.jsx
@@ -0,0 +1,10 @@
+import '../styles/main.scss'
+
+const HistoryServices = () => {
+  return (
+    <div>
+      <h1>Pago de servicios </h1>
+    </div>
+  )
+}
+export default HistoryServices
---
+++ src/domains/dashBoard/paymentService/list/ui/composite-wrappers/src/CWhistoryservices.jsx
@@ -0,0 +1,12 @@
+import '../styles/main.scss'
+import HistoryServices from '@domains/dashBoard/paymentService/list/ui/components/historyService/src/HistoryServices'
+
+const CWhistoryservices = () => {
+  return (
+    <>
+      <HistoryServices />
+    </>
+  )
+}
+
+export default CWhistoryservices
