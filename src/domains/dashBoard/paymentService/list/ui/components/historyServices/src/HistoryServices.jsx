0dcefb6b9d22a9db6678e16d5deb96f563102f19
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-02-24 16:04:39 -0500

    renderizado de lista de servicios

--- src/domains/dashBoard/paymentService/list/ui/components/historyServices/src/HistoryServices.jsx
+++ src/domains/dashBoard/paymentService/list/ui/components/historyServices/src/HistoryServices.jsx
@@ -1,11 +1,35 @@
+'use client'
+import {useState, useEffect} from 'react'
 import '../styles/main.scss'
 import CardActivity from '@domains/cross/ui/components/cardActivity/src/CardActivity'
 import ItemServices from '@domains/dashBoard/paymentService/list/ui/components/itemServices/src/ItemServices'
 
 const Historyservices = () => {
+  const baseUrl = 'https://digitalmoney.digitalhouse.com/service'
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    fetch(baseUrl, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('Service response:', data)
+        setServices(data)
+      })
+      .catch(err => console.error('API error:', err))
+  }, [])
+
   return (
     <CardActivity size="Services">
-      <ItemServices />
+      {services.length > 0 ? (
+        services.map(service => (
+          <ItemServices key={service.id} service={service} />
+        ))
+      ) : (
+        <p>Cargando servicios...</p>
+      )}
     </CardActivity>
   )
 }
--- src/domains/dashBoard/paymentService/list/ui/components/itemServices/src/ItemServices.jsx
+++ src/domains/dashBoard/paymentService/list/ui/components/itemServices/src/ItemServices.jsx
@@ -1,9 +1,22 @@
 import '../styles/main.scss'
 
-const ItemServices = () => {
+const ItemServices = ({service}) => {
+  const defaultNameCard = 'name-card'
+
+  const {id, name, date} = service ?? {}
+
   return (
-    <div>
-      <h1>item </h1>
+    <div className={defaultNameCard}>
+      <div className={`${defaultNameCard}__containItem`}>
+        <div className={`${defaultNameCard}--containImg`}></div>
+        <div className={`${defaultNameCard}__nameActivity`}>
+          <p>{name || 'Servicio sin nombre'}</p>
+        </div>
+      </div>
+
+      <button>
+        <label>Seleccionar</label>
+      </button>
     </div>
   )
 }
