f06b165429bcda5da862884896143812b2fb79ad
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-03-06 15:00:53 -0500

    implementacion del detalle de la actividad

--- src/domains/dashBoard/activity/detail/ui/components/transactionDetailInfo/src/TransactionDetailInfo.jsx
+++ src/domains/dashBoard/activity/detail/ui/components/transactionDetailInfo/src/TransactionDetailInfo.jsx
@@ -1,15 +1,46 @@
+'use client'
 import '../styles/main.scss'
+import {useEffect, useState} from 'react'
 import Image from 'next/image'
 import CardBlack from '@domains/cross/ui/components/cardBlack'
 
+const formatDate = isoString => {
+  if (!isoString) return 'Fecha desconocida'
+
+  const date = new Date(isoString)
+  const options = {day: '2-digit', month: 'long', year: 'numeric'}
+  const formattedDate = new Intl.DateTimeFormat('es-ES', options).format(date)
+
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+
+  return `${formattedDate} a ${hours}:${minutes} hs.`
+}
+
 const TransactionDetailInfo = () => {
-  const defaultDetail = 'detail-transaction'
+  const [activity, setActivity] = useState(null)
+
+  useEffect(() => {
+    const storedActivity = localStorage.getItem('activity')
+    if (storedActivity) {
+      try {
+        const parsedActivity = JSON.parse(storedActivity)
+        setActivity(parsedActivity)
+      } catch (error) {
+        console.error('Error al parsear activity:', error)
+      }
+    }
+  }, [])
+
+  if (!activity) {
+    return <p>Cargando información...</p>
+  }
 
   return (
     <CardBlack size="Detail">
-      <div className={defaultDetail}>
-        <div className={`${defaultDetail}__statusDetail`}>
-          <div className={`${defaultDetail}__statusDetail--statusInfo`}>
+      <div className="detail-transaction">
+        <div className="detail-transaction__statusDetail">
+          <div className="detail-transaction__statusDetail--statusInfo">
             <Image
               src="/images/check.svg"
               alt="Ícono de estado"
@@ -18,24 +49,24 @@ const TransactionDetailInfo = () => {
             />
             <p className="status-title">Aprobada</p>
           </div>
-          <p className="status-date">
-            Creada el 17 de agosto 2022 a 16:34 hs.{' '}
+          <p className="status-date">Creada el {formatDate(activity?.dated)}</p>
+        </div>
+
+        <div className="detail-transaction__transferSummary">
+          <p className="summary-title">Transferencia de dinero</p>
+          <p className="summary-amount">${activity?.amount ?? '0.00'}</p>
+        </div>
+
+        <div className="detail-transaction__recipientDetail">
+          <p className="recipient-title">Le transferiste a </p>
+          <p className="recipient-amount">
+            {activity?.description ?? 'Desconocido'}
           </p>
         </div>
 
-        <div className={`${defaultDetail}__transferSummary`}>
-          <p className="summary-title">Transferencia de dinero</p>
-          <p className="summary-amount">$1.266,57</p>
-        </div>
-
-        <div className={`${defaultDetail}__recipientDetail`}>
-          <p className="recipient-title">Le transferiste a </p>
-          <p className="recipient-amount">Rodrigo Vaccaro</p>
-        </div>
-
-        <div className={`${defaultDetail}__approvalDetail`}>
+        <div className="detail-transaction__approvalDetail">
           <p className="approval-title">Número de operación</p>
-          <p className="approval-amount">27903047281</p>
+          <p className="approval-amount">{activity?.id}</p>
         </div>
       </div>
     </CardBlack>
--- src/domains/dashBoard/home/ui/components/itemActuvity/src/ItemActivity.jsx
+++ src/domains/dashBoard/home/ui/components/itemActuvity/src/ItemActivity.jsx
@@ -1,18 +1,27 @@
 import '../styles/desktop.scss'
+import {useRouter} from 'next/router'
 import NameItem from '@domains/dashBoard/home/ui/components/nameItem/src/NameItem'
 import DateActivity from '@domains/dashBoard/home/ui/components/dateActivity/src/DateActivity'
 import useAccount from '@domains/cross/core/hoocks/useAccount/src/useAccount'
 
 const ItemActivity = ({activity}) => {
-  const {dated, destination, amount} = activity ?? {}
-
+  const {dated, description, amount, id} = activity ?? {}
   const {account, user} = useAccount()
+  const router = useRouter()
+
+  const handleShowDetail = () => {
+    localStorage.setItem('activity', JSON.stringify(activity))
+    console.log('activity', activity)
+    router.push('/dashBoard/activity/detail')
+  }
 
   return (
-    <div className="contain-Item">
-      <NameItem destination={destination} />
-      <DateActivity date={dated} amount={amount} />
-    </div>
+    <button onClick={handleShowDetail}>
+      <div className="contain-Item">
+        <NameItem description={description} />
+        <DateActivity date={dated} amount={amount} />
+      </div>
+    </button>
   )
 }
 
