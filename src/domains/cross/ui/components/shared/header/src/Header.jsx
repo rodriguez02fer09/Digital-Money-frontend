880db1615347e951cd5a5e664d66548d112aa3ec
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-03-12 12:25:53 -0500

    ajustes de estilos en el header

--- src/domains/cross/ui/components/shared/header/src/Header.jsx
+++ src/domains/cross/ui/components/shared/header/src/Header.jsx
@@ -1,10 +1,7 @@
 'use client'
 import '../styles/desktop.scss'
-
 import Image from 'next/image'
 import classNames from 'classnames'
-
-import Button from '@domains/cross/ui/components/button/index'
 import Avatar from '@domains/cross/ui/components/avatar/index'
 
 const Header = ({color}) => {
@@ -13,16 +10,16 @@ const Header = ({color}) => {
     color === 'green' ? '/images/logoblack.svg' : '/images/logoverde.svg'
 
   const customClass = classNames(defaultHeader, {
-    [`${defaultHeader}--${color}`]: color,
+    [`${defaultHeader}--${color}`]: color, // Solo se agrega si `color` tiene un valor
   })
 
   return (
-    <header className={`${customClass}`}>
-      <div className={`${customClass}__containImage`}>
+    <header className={customClass}>
+      <div className={`${defaultHeader}__containImage`}>
         <Image src={imageSrc} alt="Logo" width={87} height={34} />
       </div>
 
-      <div className={`${customClass}__containButton`}>
+      <div className={`${defaultHeader}__containButton`}>
         <Avatar />
       </div>
     </header>
