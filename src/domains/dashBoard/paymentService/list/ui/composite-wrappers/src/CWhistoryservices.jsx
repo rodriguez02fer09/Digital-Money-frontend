299a1501618e51c85b90b6f0151c72ad08e1dfdb
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-03-14 12:45:56 -0500

    ajustes de header mobile

--- src/domains/dashBoard/paymentService/list/ui/composite-wrappers/src/CWhistoryservices.jsx
+++ src/domains/dashBoard/paymentService/list/ui/composite-wrappers/src/CWhistoryservices.jsx
@@ -4,14 +4,16 @@ import '../styles/main.scss'
 import {useState} from 'react'
 import SearchServices from '@domains/dashBoard/paymentService/list/ui/components/searchServices/src/SearchServices'
 import Historyservices from '@domains/dashBoard/paymentService/list/ui/components/historyServices/src/HistoryServices'
+import MobileHeader from '@domains/cross/ui/components/MobileHeader/src/MobileHeader'
 
 const CWhistoryservices = () => {
   const [searchQuery, setSearchQuery] = useState('')
 
   return (
     <>
+      {' '}
+      <MobileHeader title="Pago se servicios" />
       <SearchServices onFilter={setSearchQuery} />
-
       <Historyservices searchQuery={searchQuery} />
     </>
   )
